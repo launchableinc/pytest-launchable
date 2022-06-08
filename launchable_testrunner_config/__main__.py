b15b2c9422d1883e7acb1c4a5432477365db4d2e
ninjinkun <ninjin@mac.com>
2022-06-08 10:09:21 +0900

    feature: add launchable_testrunner_config module

---
+++ launchable_testrunner_config/__main__.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import argparse
+from launchable_cli_args import CLIArgs
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(
+        description='generate/verify launchable test runner configuration file')
+    parser.add_argument('--file', dest="file",
+                        default="launchable.conf", help='configuration file name')
+    parser.add_argument('--create', action="store_true",
+                        help='create new configuration file by standard settings')
+    parser.add_argument('--verify', action="store_true",
+                        help='verify existing configuration file')
+
+    args = parser.parse_args()
+    path = args.file
+
+    exit_code = 0
+    if args.verify:
+        if os.path.isfile(path):
+            print("verifying configuration file %s..." %
+                  (os.path.join(os.getcwd(), path)))
+            conf = CLIArgs.from_yaml(path)
+            if conf.error_counter.error_count == 0:
+                print("No obvious errors were found. Please try actual test session!")
+            else:
+                conf.error_counter.print_errors()
+                exit_code = 1
+        else:
+            print("%s does not exist." % path)
+            exit_code = 1
+    elif args.create:
+        conf = CLIArgs.auto_configure(os.getcwd())
+        conf.write_as_yaml(path)
+        print("Launchable configuration file is written to %s" %
+              (os.path.join(os.getcwd(), path)))
+    else:
+        print("one of --create or --verify must be specified")
+        exit_code = 1
+    return exit_code
+
+
+if __name__ == '__main__':
+    sys.exit(main())
