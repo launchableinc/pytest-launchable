2d89061d29b26ffb3e549d52417e58cca17ab88c
ninjinkun <ninjin@mac.com>
2022-06-09 19:37:22 +0900

    feature: rename the default config file name from `launchable.conf` to `.launchable-config.yml`

--- pytest_launchable/launchable_test_context.py
+++ pytest_launchable/launchable_test_context.py
@@ -234,7 +234,7 @@ def pytest_addoption(parser):
                     action="store",
                     dest="launchable_conf_path",
                     metavar="",
-                    default="launchable.conf",
+                    default=".launchable-config.yml",
                     help="path of launchable test configuration file")
 
 
